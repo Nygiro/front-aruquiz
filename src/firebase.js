1e7ee7619c9467a08622c61d16b85d126dae6161
nigirollup <nigirollup@gmail.com>
2020-05-07 11:13:12 +0200

    fix: add env for firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 const config = {
-  apiKey: "AIzaSyAxSlgRFdLM5WSDbyHkQIFSHtt_iaIvGks",
-  authDomain: "aruquiz.firebaseapp.com",
-  databaseURL: "https://aruquiz.firebaseio.com",
-  projectId: "aruquiz",
-  storageBucket: "aruquiz.appspot.com",
-  messagingSenderId: "1027927407561",
-  appId: "1:1027927407561:web:5563b00fcc504e52518618",
-  measurementId: "G-KL5SEL522K"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 firebase.initializeApp(config);
 export const  databaseRef = firebase.database().ref();
